1f574e1352f02983fe6397bc0f8bef24ba16bcce
shreya kumari <104058567+shreypilot@users.noreply.github.com>
2023-04-22 12:28:08 +0000

    added code of navbar and sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./styles.css";
-<<<<<<< HEAD
+
 import Navbar from './components/Navbar';
 import Sidebar from "./components/Sidebar";
 export default function App() {
@@ -7,14 +7,6 @@ export default function App() {
     <div className="App">
       <Navbar/>
       <Sidebar/>
-=======
-
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
->>>>>>> 4f7334873b99c07b1f6e6ea2cf6c196f979364e9
     </div>
   );
 }
