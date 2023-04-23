c549f76943da1bf0674539b05d64a0adba2233ad
shreya kumari <104058567+shreypilot@users.noreply.github.com>
2023-04-23 13:18:41 +0000

    added code

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,18 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function Quiz() {
-  const [selectedOption, setSelectedOption] = useState(null);
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  
-  function handleOptionChange(event) {
-    setSelectedOption(event.target.value);
-  }
-  
-  function handleSubmit(event) {
-    event.preventDefault();
-    setIsSubmitted(true);
-  }
-  
+ 
   return (
     <div>
      QUIZ
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,13 +4,12 @@ import { FaHome, FaChalkboardTeacher } from 'react-icons/fa';
 import { GoFileSubmodule } from 'react-icons/go';
 import ModuleSidebar from './ModuleSidebar';
 import { Link } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { resetCaret } from '../Redux/slice/homeSlice';
+// import { useDispatch } from 'react-redux';
+// import { resetCaret } from '../Redux/slice/homeSlice';
 
 function Sidebar() {
   const [selected, setSelected] = useState('home');
   const [showModules, setShowModules] = useState(false);
-  const dispatch = useDispatch();
 
      
   const handleItemClick = (item) => {
@@ -21,11 +20,7 @@ function Sidebar() {
       setShowModules(false);
     }
   };
-   const handleLinkClick = (event) => {
-    event.preventDefault();
-    dispatch(resetCaret());
-};
-
+ 
 
   return (
     <div className="container">
@@ -41,14 +36,14 @@ function Sidebar() {
           </li>
           <li  className={selected === 'module' ? 'selected' : ''}
             onClick={() => handleItemClick('module')}>
-              <Link to="pythonloops" className="nav-link"  onClick={handleLinkClick}>
+              <Link to="pythonloops" className="nav-link" >
                 <GoFileSubmodule size={25} color="black" />
                 <span className="nav-text">MODULES</span>
               </Link>
           </li>
           <li  className={selected === 'instructor' ? 'selected' : ''}
             onClick={() => handleItemClick('instructor')}>
-            <Link to="/" className="nav-link"  onClick={handleLinkClick}>
+            <Link to="/" className="nav-link" >
             <FaChalkboardTeacher size={25} />
             <span className="nav-text">INSTRUCTORS</span>
             </Link>
