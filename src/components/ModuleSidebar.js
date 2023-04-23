4c38efa1d7eadaea37add054a8ee81ef57ed78c0
shreya kumari <104058567+shreypilot@users.noreply.github.com>
2023-04-23 13:25:43 +0000

    added code

--- src/components/ModuleSidebar.js
+++ src/components/ModuleSidebar.js
@@ -6,19 +6,12 @@ import { TbCodeCircle2 } from 'react-icons/tb';
 import { IoIosPlayCircle } from 'react-icons/io';
 import {BsQuestionCircleFill} from 'react-icons/bs';
 import {Link} from 'react-router-dom';
-import { useDispatch,useSelector } from 'react-redux';
-import { resetCaret } from '../Redux/slice/homeSlice';
+// import { useDispatch } from 'react-redux';
+// import { resetCaret } from '../Redux/slice/homeSlice';
 
 function ModuleSidebar() {
   const [selected, setSelected] = useState('python loops');
   const [hovered, setHovered] = useState('');
-  const { isCaretUp,isCaretDown } = useSelector(state => state.home);
-  const dispatch = useDispatch();
-
- const handleLinkClick = (event) => {
-  event.preventDefault();
-  dispatch(resetCaret());
-};
   const handleItemClick = (item) => {
     setSelected(item);
   };
@@ -41,7 +34,7 @@ function ModuleSidebar() {
             onMouseEnter={() => handleItemHover('python loops')}
             onMouseLeave={() => handleItemHover('')}
           >
-            <Link to="pythonloops" className="nav-link"  onClick={handleLinkClick}>
+            <Link to="pythonloops" className="nav-link"  >
             {selected === 'python loops' || hovered === 'python loops' ? (
               <IoIosPlayCircle size={23}  />
             ) : (
@@ -60,7 +53,7 @@ function ModuleSidebar() {
             onMouseEnter={() => handleItemHover('Quiz 1: Data types')}
             onMouseLeave={() => handleItemHover('')}
           >
-          <Link to="quiz" className="nav-link"  onClick={handleLinkClick}>
+          <Link to="quiz" className="nav-link"  >
 
             {selected === 'Quiz 1: Data types' || hovered === 'Quiz 1: Data types' ? (
               <BsQuestionCircleFill size={23}  />
@@ -81,7 +74,7 @@ function ModuleSidebar() {
             onMouseEnter={() => handleItemHover('Assignment-1 : operators|loops')}
             onMouseLeave={() => handleItemHover('')}
           >
-            <Link to="assignment" className="nav-link"  onClick={handleLinkClick}>
+            <Link to="assignment" className="nav-link" >
             {selected === 'Assignment-1 : operators|loops' ||
             hovered === 'Assignment-1 : operators|loops' ? (
               <TbCodeCircle2 size={23} color="black" />
